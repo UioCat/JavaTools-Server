a7e2d687f0fd3c527784617719489f6eaa25197d
VirtualMe <465132131@qq.com>
2021-06-04 19:41:44 +0800

    使用模版替换了select、update、deleteSQL语句

--- src/main/java/com/example/java_tools/service/impl/SQLConvertServiceImpl.java
+++ src/main/java/com/example/java_tools/service/impl/SQLConvertServiceImpl.java
@@ -25,103 +25,101 @@ public class SQLConvertServiceImpl {
     @Autowired
     VelocityTemplateForSQL velocityTemplateForSQL;
 
-
+    /**
+     * 创建数据库命令
+     * @param parameterMessage parameter,keyParameter,tbName
+     * @return 创建数据库命令
+     */
     public BackMessage createSqlService(ParameterMessage parameterMessage) {
 
         List<String> parameterType = new ArrayList<>();
         List<String> parameterName = new ArrayList<>();
 
-        for (String parameter : parameterMessage.getParameter()) {
+        for (String parameter: parameterMessage.getParameter()) {
             parameterType.add(parse.typeConvertForMysql(parameter.split(" ")[0]));
             parameterName.add(parse.upperToLower(parameter.split(" ")[1]));
         }
 
-        String SQLCommand = velocityTemplateForSQL.createSQLTemplate(parameterType,parameterName,parameterMessage.getTbName());
+        String SQLCommand = velocityTemplateForSQL.createSQLTemplate(parameterType, parameterName, parameterMessage.getTbName());
 
-        return new BackMessage<>(BackEnum.REQUEST_SUCCESS,SQLCommand);
+        return new BackMessage<>(BackEnum.REQUEST_SUCCESS, SQLCommand);
     }
 
-    // todo 使用模版替换 update SQL语句
+
     /**
      * 更新数据库表命令
      * @param parameterMessage parameter,keyParameter,tbName
-     * @return
+     * @return 更新数据库命令
      */
     public BackMessage updateTableService(ParameterMessage parameterMessage) {
 
-        /*
-        需求改的参数
-         */
+        // 需求改的参数
         List<String> parameterType = new ArrayList<>();
         List<String> parameterName = new ArrayList<>();
 
-        /*
-        用于判断的参数
-         */
+        // 用于判断的参数
         List<String> keyParameterType = new ArrayList<>();
         List<String> keyParameterName = new ArrayList<>();
 
-        for(String parameter : parameterMessage.getKeyParameter()){
+        for (String parameter: parameterMessage.getKeyParameter()) {
             keyParameterType.add(parameter.split(" ")[0]);
             keyParameterName.add(parameter.split(" ")[1]);
         }
-        for (String parameter : parameterMessage.getParameter()) {
+        for (String parameter: parameterMessage.getParameter()) {
             parameterType.add(parameter.split(" ")[0]);
             parameterName.add(parameter.split(" ")[1]);
         }
 
-        String updateSQL = sqlProduceService.composeSqlUpdate(keyParameterType,keyParameterName,
-                                                  parameterType,parameterName,
-                                                  parameterMessage.getTbName());
-
-        return new BackMessage<>(BackEnum.REQUEST_SUCCESS,updateSQL);
+        String updateSQL = velocityTemplateForSQL.updateSQLTemplate(parameterType, parameterName,
+                keyParameterType, keyParameterName, parameterMessage.getTbName());
+        return new BackMessage<>(BackEnum.REQUEST_SUCCESS, updateSQL);
     }
 
     /**
      * 插入数据库命令
      * @param parameterMessage parameter，tbName
-     * @return 参入数据库命令
+     * @return 插入数据库命令
      */
-    public BackMessage insertMsgService(ParameterMessage parameterMessage){
+    public BackMessage insertMsgService(ParameterMessage parameterMessage) {
 
         List<String> parameterName = new ArrayList<>();
 
-        for(String parameter : parameterMessage.getParameter()){
+        for (String parameter: parameterMessage.getParameter()) {
             parameterName.add(parse.upperToLower(parameter.split(" ")[1]));
+
         }
 
         String insertSQL = velocityTemplateForSQL.insertSQLTemplate(parameterName, parameterMessage.getTbName());
-        return new BackMessage<>(BackEnum.REQUEST_SUCCESS,insertSQL);
+        return new BackMessage<>(BackEnum.REQUEST_SUCCESS, insertSQL);
     }
 
-    // todo 使用模版替换 delete SQL语句
     /**
      * 生成删除信息命令
      * @param parameterMessage keyParameter,tbName
      * @return 删除信息命令
      */
-    public BackMessage deleteMsg(ParameterMessage parameterMessage){
+    public BackMessage deleteMsg(ParameterMessage parameterMessage) {
 
         List<String> keyParameterType = new ArrayList<>();
         List<String> keyParameterName = new ArrayList<>();
 
-        for(String keyParameter : parameterMessage.getKeyParameter()){
+        for (String keyParameter: parameterMessage.getKeyParameter()) {
             keyParameterType.add(keyParameter.split(" ")[0]);
             keyParameterName.add(keyParameter.split(" ")[1]);
         }
 
-        String deleteSQL = sqlProduceService.composeSQLDelete(keyParameterType,keyParameterName,parameterMessage.getTbName());
+        String deleteSQL = velocityTemplateForSQL.deleteSQLTemplate(keyParameterType, keyParameterName, parameterMessage.getTbName());
 
-        return new BackMessage<>(BackEnum.REQUEST_SUCCESS,deleteSQL);
+        return new BackMessage<>(BackEnum.REQUEST_SUCCESS, deleteSQL);
     }
 
-    // todo 使用模版替换 select SQL语句
+
     /**
      * 查询信息命令生成
      * @param parameterMessage parameter，keyParameter，tbName
      * @return 查询数据库信息命令
      */
-    public BackMessage selectMsg(ParameterMessage parameterMessage){
+    public BackMessage selectMsg(ParameterMessage parameterMessage) {
 
         List<String> keyParameterType = new ArrayList<>();
         List<String> keyParameterName = new ArrayList<>();
@@ -129,19 +127,18 @@ public class SQLConvertServiceImpl {
         List<String> parameterName = new ArrayList<>();
 
 
-        for(String keyParameter : parameterMessage.getKeyParameter()){
+        for (String keyParameter: parameterMessage.getKeyParameter()) {
             keyParameterType.add(keyParameter.split(" ")[0]);
             keyParameterName.add(keyParameter.split(" ")[1]);
         }
 
-        for(String parameter : parameterMessage.getParameter()){
+        for (String parameter: parameterMessage.getParameter()) {
             parameterName.add(parameter.split(" ")[1]);
         }
 
-        String selectSQL = sqlProduceService.composeSQLSelect(parameterName,
-                keyParameterType,keyParameterName,parameterMessage.getTbName());
+        String selectSQL = velocityTemplateForSQL.selectSQLTemplate(parameterName,keyParameterType,keyParameterName,parameterMessage.getTbName());
 
-        return new BackMessage<>(BackEnum.REQUEST_SUCCESS,selectSQL);
+        return new BackMessage<>(BackEnum.REQUEST_SUCCESS, selectSQL);
 
     }
 
