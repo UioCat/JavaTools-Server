c638aee59b2866fbc97836784a68f774e754f193
Uio <406453373@qq.com>
2021-06-04 15:26:24 +0800

    Revert "新增测试类（空）"
    This reverts commit f5447790

--- src/test/java/com/example/java_tools/controller/TokenizerControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.java_tools.controller;
-
-/**
- * @author han xun
- * Date 2021/6/4 下午1:47
- * Description:
- */
-public class TokenizerControllerTest {
-}
--- src/test/java/com/example/java_tools/manager/ParseStrManagerTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.java_tools.manager;
-
-/**
- * @author han xun
- * Date 2021/6/4 下午1:46
- * Description:
- */
-public class ParseStrManagerTest {
-}
