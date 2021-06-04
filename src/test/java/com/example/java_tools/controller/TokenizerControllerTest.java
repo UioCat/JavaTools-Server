f544779030e979d7c00d6812d0d0be38c015d9ca
Uio <406453373@qq.com>
2021-06-04 14:59:05 +0800

    新增测试类（空）

---
+++ src/test/java/com/example/java_tools/controller/TokenizerControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.java_tools.controller;
+
+/**
+ * @author han xun
+ * Date 2021/6/4 下午1:47
+ * Description:
+ */
+public class TokenizerControllerTest {
+}
---
+++ src/test/java/com/example/java_tools/service/TokenizerServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.java_tools.service;
+
+/**
+ * @author han xun
+ * Date 2021/6/4 下午1:46
+ * Description:
+ */
+public class TokenizerServiceTest {
+}
