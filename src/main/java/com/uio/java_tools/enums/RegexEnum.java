df9c6c35477a64019ef02152e7bb4fedcbcb0463
VM <465132131@qq.com>
2021-11-06 17:36:54 +0800

    :bug: 修复Java注释解析功能

--- src/main/java/com/uio/java_tools/enums/RegexEnum.java
+++ src/main/java/com/uio/java_tools/enums/RegexEnum.java
@@ -9,7 +9,7 @@ public enum RegexEnum {
     /**
      * field解析正则表达式
      */
-    TYPE_FIELD_REGEX("(integer|string|boolean|double|float|int|byte|short|long|char|character|date) (\\w|=| |\")*;"),
+    TYPE_FIELD_REGEX("(public |private |protected )*(integer|string|boolean|double|float|int|byte|short|long|char|character|date) (\\w|=| |\")*;"),
     /**
      * 解析方法正则表达式
      */
@@ -17,7 +17,7 @@ public enum RegexEnum {
     /**
      * parameter解析正则表达式
      */
-    PARAMETER_REGEX("(public |private |protected )*(integer|string|boolean|double|float|int|byte|short|long|char|character|date) (\\w|=| |\")*;");
+    PARAMETER_REGEX("/[\\w\\* ].*?;|(\\w|=| |\")*;");
 
 
     private String regexString;
